0a8bb81775af88080b1bb602357404e0de9a5582
kamil3242 <166690478+kamil3242@users.noreply.github.com>
2024-04-30 12:51:17 +0200

    wip: add user printing functionality

---
+++ main.py
@@ -0,0 +1,17 @@
+imię="Dawid"
+nazwisko="Bałuka"
+liczba_postów=6000
+
+print(f"twoj znajomy {imię} {nazwisko} opublikowal {liczba_postów} postów")
+
+imię_2="Kewin"
+nazwisko_2="Czajkowski"
+liczba_postów_2=6002
+
+print(f"twoj znajomy {imię_2} {nazwisko_2} opublikowal {liczba_postów_2} postów")
+
+imię_3="Kamil"
+nazwisko_3="Gil"
+liczba_postów_3=1_000_000
+
+print(f"twoj znajomy {imię_3} {nazwisko_3} opublikowal {liczba_postów_3} postów")
