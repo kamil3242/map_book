23067dfd39b4b4ad9000b60114774bd7c88b684c
kamil3242 <166690478+kamil3242@users.noreply.github.com>
2024-04-30 14:57:26 +0200

    wip: add a function to create user

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from models.data import users
-from utils.crud import show_users
+from utils.crud import show_users,add_new_user
 
 
 
 if __name__ == "__main__":
     print("witaj użytkowniku")
     while True:
-        print("wybierz opcję menu:")
+        print("Menu:")
+        print("0. zakończ program")
         print("1. wyświetl co u znajomych")
+        print("2. dodaj użytkownika")
         menu_option:str = input("dokonaj wyboru:")
-        if menu_option == "o":
+        if menu_option == "0":
             print("program kończy pracę")
             break
         if menu_option == "1":
             show_users(users)
+        if menu_option == "2":
+            add_new_user(users)
--- utils/crud.py
+++ utils/crud.py
@@ -1,3 +1,11 @@
 def show_users(user_list: list[dict]) -> None:
     for user in user_list:
         print(f"twój znajomy {user['name']} opublikował: {user['posts']} postów")
+
+def add_new_user(users: list)-> None:
+    new_name: str = input("Enter your name: ")
+    new_surname: str = input("Enter your surname: ")
+    new_posts: str = input("Enter your posts: ")
+    new_user: dict = {"name": new_name, "surname": new_surname, "posts": new_posts}
+    print(new_user)
+    users.append(new_user)
