8164b202a9c9aa13adc16267088682d24a30099c
kamil3242 <166690478+kamil3242@users.noreply.github.com>
2024-04-30 13:00:41 +0200

    wip: prepare a for loop

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-imię="Dawid"
-nazwisko="Bałuka"
-liczba_postów=6000
+users: list[dict] = [
+    {"name": "Dawid", "surname": "Bałuka", "posts": 6000},
+    {"name": "Kewin", "surname": "Czajkowski", "posts": 6002},
+    {"name": "Kamil", "surname": "Gil", "posts": 1_000_000},
+    {"name": "Daniel", "surname": "Błaszczyk", "posts": 6}
 
-print(f"twoj znajomy {imię} {nazwisko} opublikowal {liczba_postów} postów")
+]
 
-imię_2="Kewin"
-nazwisko_2="Czajkowski"
-liczba_postów_2=6002
-
-print(f"twoj znajomy {imię_2} {nazwisko_2} opublikowal {liczba_postów_2} postów")
-
-imię_3="Kamil"
-nazwisko_3="Gil"
-liczba_postów_3=1_000_000
-
-print(f"twoj znajomy {imię_3} {nazwisko_3} opublikowal {liczba_postów_3} postów")
+for user in users:
+    print(f"twój znajomy {user['name']} opublikował: {user['posts']} postów")
